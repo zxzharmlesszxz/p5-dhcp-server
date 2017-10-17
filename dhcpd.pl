99e46bc870ea90ce798acf27562fc8b357cb57e2
Harmless <harmless@syneforge.com>
2017-10-17 18:19:52 +0300

    Cleanups

--- dhcpd.pl
+++ dhcpd.pl
@@ -478,22 +478,27 @@ sub GetRelayAgentOptions($$$$$$) {
 
     for (my $i = 0; defined($RelayAgent[$i]); $i += 2){
         switch ($RelayAgent[$i]){
-        case 1 { # Circuit ID
-            next if (length($RelayAgent[($i + 1)]) < 4);
-            # first bytes must be: 00 04
-            $_[1] = unpack('n', substr($RelayAgent[($i + 1)], -4, 2)); # may be 's'
-            $_[2] = unpack('C', substr($RelayAgent[($i + 1)], -2, 1));
-            $_[3] = unpack('C', substr($RelayAgent[($i + 1)], -1, 1));
+            case 1 { # Circuit ID
+                next if (length($RelayAgent[($i + 1)]) < 4);
+                # first bytes must be: 00 04
+                $_[1] = unpack('n', substr($RelayAgent[($i + 1)], -4, 2)); # may be 's'
+                logger("RelayAgent: ".$_[1]);
+                $_[2] = unpack('C', substr($RelayAgent[($i + 1)], -2, 1));
+                logger("RelayAgent: ".$_[2]);
+                $_[3] = unpack('C', substr($RelayAgent[($i + 1)], -1, 1));
+                logger("RelayAgent: ".$_[3]);
             }
-        case 2 { # Remote ID
-            next if (length($RelayAgent[($i + 1)]) < 6);
-            # first bytes must be: 00 06 or 01 06 or 02 xx
-            # first digit - format/data type, second - len
-            $_[4] = FormatMAC(unpack("H*", substr($RelayAgent[($i + 1)], -6, 6)));
-            # 02 xx - contain vlan num, undone
+            case 2 { # Remote ID
+                next if (length($RelayAgent[($i + 1)]) < 6);
+                # first bytes must be: 00 06 or 01 06 or 02 xx
+                # first digit - format/data type, second - len
+                $_[4] = FormatMAC(unpack("H*", substr($RelayAgent[($i + 1)], -6, 6)));
+                logger("RelayAgent: ".$_[4]);
+                # 02 xx - contain vlan num, undone
             }
-        case 6 { # Subscriber ID
-            $_[5] = $RelayAgent[($i + 1)];
+            case 6 { # Subscriber ID
+                $_[5] = $RelayAgent[($i + 1)];
+                logger("RelayAgent: ".$_[5]);
             }
         }
     }
